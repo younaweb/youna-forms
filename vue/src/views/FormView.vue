4462c8233512509cebcaf40739aab4e820a2a70c
YoussefHarizi <youssef.harizi@gmail.com>
2022-03-10 20:08:22 +0100

    create form front part1

--- vue/src/views/FormView.vue
+++ vue/src/views/FormView.vue
@@ -1,40 +1,173 @@
 <template>
- <page-component>
- <template v-slot:header>
- <h1  class="text-3xl font-bold text-gray-900">
- {{route.params.id? model.title:'Create Form'}}
- </h1>
-<button v-if="route.params.id" class="bg-red-500 flex text-white px-32 py-3 rounded-md text-1xl font-medium hover:bg-red-700 transition duration-300">
- <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-</svg>
-delete form
-</button>
- </template>
- </page-component>
+    <page-component>
+        <template v-slot:header>
+            <h1 class="text-3xl font-bold text-gray-900">
+                {{ route.params.id ? model.title : "Create Form" }}
+            </h1>
+            <button
+                v-if="route.params.id"
+                class="bg-red-500 flex text-white px-32 py-3 rounded-md text-1xl font-medium hover:bg-red-700 transition duration-300"
+            >
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    stroke-width="2"
+                >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                    />
+                </svg>
+                delete form
+            </button>
+        </template>
+
+        <!-- form -->
+        <form action="" class="w-1/2 mx-auto">
+            <div class="shadow sm:rounded-md sm:overflow-hidden">
+                <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
+                    <!-- image -->
+                    <div>
+                        <label class="block text-sm font-medium text-gray-700">
+                            Photo
+                        </label>
+                        <div class="mt-1 flex items-center">
+                            <span
+                                class="inline-block h-12 w-12 overflow-hidden bg-gray-100"
+                            >
+                                <svg
+                                    class="h-full w-full text-gray-300"
+                                    fill="currentColor"
+                                    viewBox="0 0 24 24"
+                                >
+                                    <path
+                                        d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z"
+                                    />
+                                </svg>
+                            </span>
+                            <button
+                                type="button"
+                                class="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 relative"
+                            >
+                                <input
+                                    type="file"
+                                    class="absolute left-0 top-0 right-0 bottom-0 opacity-0"
+                                   
+                                />
+                                Change
+                            </button>
+                        </div>
+                    </div>
+                    <!-- end image -->
+                    <!-- title -->
+                    <div class="col-span-6">
+                        <label
+                            for="title"
+                            class="block text-sm font-medium text-gray-700"
+                            >Title</label
+                        >
+                        <input
+                            type="text"
+                            name="title"
+                            id="title"
+                            autocomplete="title"
+                             v-model="model.title"
+                            class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                        />
+                    </div>
+                    <!-- end title -->
+                    <!-- description -->
+                    <div>
+                        <label
+                            for="description"
+                            class="block text-sm font-medium text-gray-700"
+                        >
+                            Description
+                        </label>
+                        <div class="mt-1">
+                            <textarea
+                                id="description"
+                                name="description"
+                                 v-model="model.description"
+                                rows="3"
+                                class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
+                                placeholder="tape description here"
+                            />
+                        </div>
+                    </div>
+                    <!-- end description -->
+                    <!-- date expiration -->
+                    <div class="col-span-6">
+                        <label
+                            for="title"
+                            class="block text-sm font-medium text-gray-700"
+                            >Date</label
+                        >
+                        <input
+                            type="date"
+                            name="title"
+                            id="title"
+                             v-model="model.expire_date"
+                            autocomplete="title"
+                            class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                        />
+                    </div>
+                    <!-- end date expiration -->
+                    <!-- active -->
+                    <div class="col-span-6">
+                        <input
+                            id="active"
+                            name="active"
+                            type="checkbox"
+                             v-model="model.status"
+                            class="focus:ring-indigo-500 h-5 w-5 text-indigo-600 border-gray-300 rounded"
+                        />
+                        <label
+                            for="active"
+                            class="text-sm font-medium text-gray-700 ml-2"
+                            >Active</label
+                        >
+                    </div>
+
+                    <!-- end active -->
+                    <!-- save -->
+                    <div class="px-4 py-3  text-right sm:px-6">
+                        <button
+                            type="submit"
+                            class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        >
+                            Save
+                        </button>
+                    </div>
+                    <!-- end save -->
+                </div>
+            </div>
+        </form>
+    </page-component>
 </template>
 
 <script setup>
-import PageComponent from '../components/PageComponent.vue'
-import { ref } from 'vue';
-import { useRouter,useRoute} from 'vue-router'
-const route=useRoute()
-const router=useRouter()
+import PageComponent from "../components/PageComponent.vue";
+import { ref } from "vue";
+import { useRouter, useRoute } from "vue-router";
+const route = useRoute();
+const router = useRouter();
 
 // Create empty survey
 let model = ref({
-  title: "",
-  slug: "",
-  status: false,
-  description: null,
-  image: null,
-  image_url: null,
-  expire_date: null,
-  questions: [],
+    title: "",
+    slug: "",
+    status: false,
+    description: null,
+    image: null,
+    image_url: null,
+    expire_date: null,
+    questions: [],
 });
-
 </script>
 
-<style>
-
-</style>
+<style></style>
--- vue/src/views/Forms.vue
+++ vue/src/views/Forms.vue
@@ -1,118 +1,188 @@
 <template>
- <page-component>
- <template v-slot:header>
- <h1  class="text-3xl font-bold text-gray-900">Forms</h1>
-<router-link :to="{name:'FormCreate'}" class="bg-blue-500 text-white px-32 py-3 rounded-md text-1xl font-medium hover:bg-blue-700 transition duration-300">
- <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-4 w-4 -mt-1 inline-block"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M12 4v16m8-8H4"
-            />
-          </svg>
-New Form
-</router-link>
- </template>
-   <div class=" px-4 py-4 max-w-xl my-auto w-1/3">
-    <div class="bg-gray-50 md:bg-white md:shadow-xl rounded-lg mb-6 ">
-      
-      <a target="_blank" rel="noreferrer noopener" href="https://www.wionews.com/science/astronomer-clicks-photo-of-potentially-hazardous-asteroid-coming-towards-earth-451098">
-        <div class="md:flex-shrink-0">
+    <page-component>
+        <template v-slot:header>
+            <h1 class="text-3xl font-bold text-gray-900">Forms</h1>
+            <router-link
+                :to="{ name: 'FormCreate' }"
+                class="bg-blue-500 text-white px-32 py-3 rounded-md text-1xl font-medium hover:bg-blue-700 transition duration-300"
+            >
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-4 w-4 -mt-1 inline-block"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M12 4v16m8-8H4"
+                    />
+                </svg>
+                New Form
+            </router-link>
+        </template>
+        <div class="flex">
+            <div class="px-4 py-4 max-w-xl my-auto w-1/3">
+                <div
+                    class="bg-gray-50 md:bg-white md:shadow-xl rounded-lg mb-6"
+                >
+                    <a
+                        target="_blank"
+                        rel="noreferrer noopener"
+                        href="https://www.wionews.com/science/astronomer-clicks-photo-of-potentially-hazardous-asteroid-coming-towards-earth-451098"
+                    >
+                        <div class="md:flex-shrink-0">
+                            <img
+                                src="https://cdn.wionews.com/sites/default/files/styles/story_page/public/2022/01/30/237315-asteroid-1.jpg"
+                                alt="uploaded cover image"
+                                class="object-cover h-full w-full rounded-lg rounded-b-none"
+                            />
+                        </div>
+                    </a>
 
-          <img src="https://cdn.wionews.com/sites/default/files/styles/story_page/public/2022/01/30/237315-asteroid-1.jpg" alt="uploaded cover image"
-            class="object-cover h-full w-full rounded-lg rounded-b-none">
+                    <div class="py-1">
+                        <div class="p-4">
+                            <h2
+                                class="truncate font-bold mb-2 md:mt-4 text-2xl text-gray-800 tracking-normal"
+                            >
+                                Astronomer clicks photo of potentially hazardous
+                                asteroid coming towards Earth - WION
+                            </h2>
 
-        </div>
-      </a>
-      
-      <div class="py-1">
-        <div class="p-4">
-          <h2 class="truncate font-bold mb-2 md:mt-4 text-2xl text-gray-800 tracking-normal">Astronomer clicks photo of potentially hazardous asteroid coming towards Earth - WION</h2>
-          
-          <p class="break-words text-sm text-gray-700 px-2 mr-1">Human eyes (and lenses) are almost always pointed at the sky. Along with wonders of the universe, they are also on the look-out for threats that may be hurtling towards Earth at speeds of thousands of miles an hour.</p>
-          
-        </div>
+                            <p
+                                class="break-words text-sm text-gray-700 px-2 mr-1"
+                            >
+                                Human eyes (and lenses) are almost always
+                                pointed at the sky. Along with wonders of the
+                                universe, they are also on the look-out for
+                                threats that may be hurtling towards Earth at
+                                speeds of thousands of miles an hour.
+                            </p>
+                        </div>
 
-        <div class="flex items-center justify-between p-2 md:p-4 md:mx-4">
-          <a target="_blank" rel="noreferrer noopener" href="https://www.wionews.com/science/astronomer-clicks-photo-of-potentially-hazardous-asteroid-coming-towards-earth-451098">
-            <div class="flex items-center">
-              <div class="text-sm ml-2">
-                
-                <p class="text-black leading-none">WION</p>
-                
-
-                
-                <p class="text-gray-700">February 6, 2022</p>
-                
-              </div>
+                        <div
+                            class="flex items-center justify-end p-2 md:p-4 md:mx-4"
+                        >
+                            
+                            <a
+                                class="text-green-500 p-2 cursor-pointer rounded-full hover:bg-green-200 transition duration-300"
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    class="h-8 w-8"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                    stroke-width="2"
+                                >
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+                                    />
+                                </svg>
+                            </a>
+                            <a
+                                class="text-red-500 p-2 cursor-pointer rounded-full hover:bg-red-200 transition duration-300"
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    class="h-8 w-8"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                    stroke-width="2"
+                                >
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                                    />
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </a>
-          <a target="_blank" rel="noreferrer noopener" href="https://www.wionews.com/science/astronomer-clicks-photo-of-potentially-hazardous-asteroid-coming-towards-earth-451098"
-            class="bg-green-500 hover:bg-green-600 text-white rounded-full px-8 py-2">
-            Read More
-          </a>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- *********************** -->
-    <div class=" px-4 py-4 max-w-xl my-auto w-1/3">
-    <div class="bg-gray-50 md:bg-white md:shadow-xl rounded-lg mb-6 ">
-      
-      <a target="_blank" rel="noreferrer noopener" href="https://www.wionews.com/science/astronomer-clicks-photo-of-potentially-hazardous-asteroid-coming-towards-earth-451098">
-        <div class="md:flex-shrink-0">
+            <!-- *********************** -->
+            <div class="px-4 py-4 max-w-xl my-auto w-1/3">
+                <div
+                    class="bg-gray-50 md:bg-white md:shadow-xl rounded-lg mb-6"
+                >
+                    <a
+                        target="_blank"
+                        rel="noreferrer noopener"
+                        href="https://www.wionews.com/science/astronomer-clicks-photo-of-potentially-hazardous-asteroid-coming-towards-earth-451098"
+                    >
+                        <div class="md:flex-shrink-0">
+                            <img
+                                src="https://cdn.wionews.com/sites/default/files/styles/story_page/public/2022/01/30/237315-asteroid-1.jpg"
+                                alt="uploaded cover image"
+                                class="object-cover h-full w-full rounded-lg rounded-b-none"
+                            />
+                        </div>
+                    </a>
 
-          <img src="https://cdn.wionews.com/sites/default/files/styles/story_page/public/2022/01/30/237315-asteroid-1.jpg" alt="uploaded cover image"
-            class="object-cover h-full w-full rounded-lg rounded-b-none">
+                    <div class="py-1">
+                        <div class="p-4">
+                            <h2
+                                class="truncate font-bold mb-2 md:mt-4 text-2xl text-gray-800 tracking-normal"
+                            >
+                                Astronomer clicks photo of potentially hazardous
+                                asteroid coming towards Earth - WION
+                            </h2>
 
-        </div>
-      </a>
-      
-      <div class="py-1">
-        <div class="p-4">
-          <h2 class="truncate font-bold mb-2 md:mt-4 text-2xl text-gray-800 tracking-normal">Astronomer clicks photo of potentially hazardous asteroid coming towards Earth - WION</h2>
-          
-          <p class="break-words text-sm text-gray-700 px-2 mr-1">Human eyes (and lenses) are almost always pointed at the sky. Along with wonders of the universe, they are also on the look-out for threats that may be hurtling towards Earth at speeds of thousands of miles an hour.</p>
-          
-        </div>
+                            <p
+                                class="break-words text-sm text-gray-700 px-2 mr-1"
+                            >
+                                Human eyes (and lenses) are almost always
+                                pointed at the sky. Along with wonders of the
+                                universe, they are also on the look-out for
+                                threats that may be hurtling towards Earth at
+                                speeds of thousands of miles an hour.
+                            </p>
+                        </div>
 
-        <div class="flex items-center justify-between p-2 md:p-4 md:mx-4">
-          <a target="_blank" rel="noreferrer noopener" href="https://www.wionews.com/science/astronomer-clicks-photo-of-potentially-hazardous-asteroid-coming-towards-earth-451098">
-            <div class="flex items-center">
-              <div class="text-sm ml-2">
-                
-                <p class="text-black leading-none">WION</p>
-                
+                        <div
+                            class="flex items-center justify-between p-2 md:p-4 md:mx-4"
+                        >
+                            <a
+                                target="_blank"
+                                rel="noreferrer noopener"
+                                href="https://www.wionews.com/science/astronomer-clicks-photo-of-potentially-hazardous-asteroid-coming-towards-earth-451098"
+                            >
+                                <div class="flex items-center">
+                                    <div class="text-sm ml-2">
+                                        <p class="text-black leading-none">
+                                            WION
+                                        </p>
 
-                
-                <p class="text-gray-700">February 6, 2022</p>
-                
-              </div>
+                                        <p class="text-gray-700">
+                                            February 6, 2022
+                                        </p>
+                                    </div>
+                                </div>
+                            </a>
+                            <a
+                                target="_blank"
+                                rel="noreferrer noopener"
+                                href="https://www.wionews.com/science/astronomer-clicks-photo-of-potentially-hazardous-asteroid-coming-towards-earth-451098"
+                                class="bg-green-500 hover:bg-green-600 text-white rounded-full px-8 py-2"
+                            >
+                                Read More
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </a>
-          <a target="_blank" rel="noreferrer noopener" href="https://www.wionews.com/science/astronomer-clicks-photo-of-potentially-hazardous-asteroid-coming-towards-earth-451098"
-            class="bg-green-500 hover:bg-green-600 text-white rounded-full px-8 py-2">
-            Read More
-          </a>
         </div>
-      </div>
-    </div>
-  </div>
- </page-component>
+    </page-component>
 </template>
 
 <script setup>
-import PageComponent from '../components/PageComponent.vue'
-
+import PageComponent from "../components/PageComponent.vue";
 </script>
 
-<style>
-
-</style>
+<style></style>
